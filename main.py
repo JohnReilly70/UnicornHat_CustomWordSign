ca7c0e8f1210f7f9f22edbfed758ce7bab079489
SgtSafety <johnreilly1989@hotmail.co.uk>
2016-07-03 15:33:09 +0100

    added simple userinput,string to list convertor and main function

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
+'''
+Importing Librarys required for use of unicornhat
+    -unicornhat
+    -time, to allow wait states
+    -characterLibrary has characters created in a 8x8 matrix, will check message to make sure all characters available
+'''
 
-import unicornhat as UH
+'''import unicornhat as UH
 import time
-import characterLibrary
+import characterLibrary'''
 
-for coords in characterLibrary.A:
-    x, y = coords
-    UH.set_pixel(x,y,255,0,0)
-UH.show()
+def userInput():
+    '''
+    Simple userInput function to keep the design simple on the console
+    '''
+    Message = str(input("\nInput: "))
+    return  Message
 
-time.sleep(5)
+def messageConvertToList(msg):
+    '''
+    Converts a string to a list
+    :param msg: string message
+    :return: list with all characters including whitespace
+    '''
+    listMsg = []
+    for char in msg:
+        listMsg.append(char)
+    return listMsg
 
+def main():
+    print(messageConvertToList("Hello i am john"))
+    pass
 
-
-
-
-
-
-
-
-
-
+main()
